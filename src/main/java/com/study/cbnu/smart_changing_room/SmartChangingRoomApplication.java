3c84172893e28c1e2f6469065ffbf5462462c486
aekik1118 <aekik1118@naver.com>
2019-11-13 12:13:47 +0900

    init

---
+++ src/main/java/com/study/cbnu/smart_changing_room/SmartChangingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.study.cbnu.smart_changing_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartChangingRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartChangingRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/cbnu/smart_changing_room/SmartChangingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.cbnu.smart_changing_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartChangingRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
